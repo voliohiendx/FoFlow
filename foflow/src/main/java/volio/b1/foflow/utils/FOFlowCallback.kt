e03322ff4e86afc090344287deccd06fdcb20d8e
HienDX21 <hiendx@govo.tech>
2025-11-17 10:43:22 +0700

    update onboarding

--- foflow/src/main/java/volio/b1/foflow/utils/FOFlowCallback.kt
+++ foflow/src/main/java/volio/b1/foflow/utils/FOFlowCallback.kt
@@ -6,10 +6,10 @@ import androidx.lifecycle.Lifecycle
 
 interface FOFlowCallback {
     fun showNativeAds(spaceName: String, viewGroup: ViewGroup, idLayoutAds: Int, screenName: String)
-    fun pushTracking(isResume: Boolean, screenName: String)
+    fun pushTracking(isResume: Boolean, screenName: String){}
     fun selectLanguage(codeLanguage: String)
     fun showInterAds(lifecycle: Lifecycle, onNextScreen: () -> Unit)
     fun isEnableShowAds(spaceName: String): Boolean
 
-    fun showApplyLanguage(isShow: Boolean, view: View)
+    fun showApplyLanguage(isShow: Boolean, view: View){}
 }
